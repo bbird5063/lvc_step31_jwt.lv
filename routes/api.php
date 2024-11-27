9dfada7c072a9b6d9b5d6c7c97cffbbefd8cd52e
bbird5063 <bbird5063@gmail.com>
2024-11-27 11:07:25 +0200

    2. Установка JWT Auth. Aсcess(Login) token и Refresh token, token expired.

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+	Route::post('login', 'App\Http\Controllers\AuthController@login');
+	Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+	Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+	Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+	Route::group(['namespace' => 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function () {
+		Route::get('/', 'IndexController');
+	});
+});
+
+
 Route::group(['namespace' => 'App\Http\Controllers\User', 'prefix' => 'users'], function () {
 	Route::post('/', 'StoreController'); /* ЗАБЫЛ ПУТЬ В 'namespace'!!! */
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function() {
-	Route::get('/', 'IndexController');
-});
 
 //Route::get('/fruits', 'App\Http\Controllers\Fruit\IndexController');
