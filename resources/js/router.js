d243c256adb71cfcaa937e90be961766cd28e07c
bbird5063 <bbird5063@gmail.com>
2024-12-01 08:48:37 +0200

    6. Используем beforeEach в router для перенаправления, если токена нет

--- resources/js/router.js
+++ resources/js/router.js
@@ -21,11 +21,46 @@ const routes = [
 		name: 'user.personal'
 	},
 
+
+
+	/* Если в адресной строке ввели страницу, которой нет */
+	{
+		path: '/:catchAll(.*)', component: () => import('./components/User/Personal.vue'),
+		name: '404' // от балды
+	},
+
 ];
+
 
 const router = new createRouter({ // работает с и без 'new'
 	routes,
 	history: createWebHistory(),
+});
+
+router.beforeEach((to, from, next) => { //to->куда, from->откуда, next->callback-фукция
+	//console.log('to.name: ' + to.name); // 'user.registration'
+	//console.log('from.name: ' + from.name); // 'user.login'
+	//console.log(next); // callback-фукция
+
+	const accessToken = localStorage.getItem('access_token'); // получаю токен, если его нет - null
+
+	if (!accessToken) {
+		if (to.name === 'user.login' || to.name === 'user.registration') {
+			return next();
+		} else {
+			return next({
+				name: 'user.login'
+			});
+		}
+	}
+
+	if (to.name == 'user.login' && accessToken) { // ЗАЧЕМ?
+		return next({
+			name: 'user.personal'
+		});
+	}
+
+	next(); // если условия if-ов не соответствуют
 });
 
 export default router;
